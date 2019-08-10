f0987debcb860dfd266fb1becd0da07c0d34ffa4
hibimosaku <n.kubomitsu@gmail.com>
2019-08-11 07:31:10 +0900

    todolist_20190810

--- js/main.js
+++ js/main.js
@@ -3,6 +3,121 @@ var tabSkill={
   data:function(){
     return{
       activeTab:"tabs-1",
+      task_progress:['すべて','完成','未完成'],
+      activeoption:"0",
+      tasks:
+      { 
+          coder:[
+              {
+                number:'1',
+                study_item:'コーディング模写①',
+                title:'メルカリ模写',
+                url:'http://task.hibimosaku.jp/',
+                is_finished:'true',
+                description:'レスポンス済み。画像は適当',
+                date:'2019/07/28',
+              },
+              {
+                number:'2',
+                study_item:'コーディング模写②',
+                title:'アップル模写',
+                url:'http://task2.hibimosaku.jp/',
+                is_finished:'true',
+                description:'レスポンス済み。画像は適当',
+                date:'2019/08/06',
+              },
+              {
+                number:'3',
+                study_item:'scss',
+                title:'-',
+                is_finished:'true',
+                description:'主に、ネスト・レスポンシブの@mixin',
+                date:'2019/07',
+              },
+          ],
+          javascript:[
+              {
+                number:'1',
+                study_item:'bbbb',
+                title:'mosaku',
+                url:'http://task.hibimosaku.jp/',
+                is_finished:'true',
+                description:'aaaaaaaaa',
+              },
+              {
+                number:'2',
+                study_item:'cccc',
+                title:'mosaku',
+                url:'http://task.hibimosaku.jp/',
+                is_finished:'false',
+                description:'aaaaaaaaa',
+              },
+              {
+                number:'3',
+                study_item:'dddd',
+                title:'mosaku',
+                url:'http://task.hibimosaku.jp/',
+                is_finished:'true',
+                description:'aaaaaaaaa',
+              },
+          ],
+          php:[
+              {
+                number:'1',
+                study_item:'eeee',
+                title:'mosaku',
+                url:'http://task.hibimosaku.jp/',
+                is_finished:'true',
+                description:'aaaaaaaaa',
+              },
+              {
+                number:'2',
+                study_item:'scss',
+                title:'mosaku',
+                url:'http://task.hibimosaku.jp/',
+                is_finished:'false',
+                description:'aaaaaaaaa',
+              },
+              {
+                number:'3',
+                study_item:'scss',
+                title:'mosaku',
+                url:'http://task.hibimosaku.jp/',
+                is_finished:'true',
+                description:'aaaaaaaaa',
+              },
+          ],
+          other:[
+            {
+              number:'1',
+              study_item:'jjjj',
+              title:'mosaku',
+              url:'http://task.hibimosaku.jp/',
+              is_finished:'true',
+              description:'aaaaaaaaa',
+            },
+            {
+              number:'2',
+              study_item:'scss',
+              title:'mosaku',
+              url:'http://task.hibimosaku.jp/',
+              is_finished:'false',
+              description:'aaaaaaaaa',
+            },
+            {
+              number:'3',
+              study_item:'scss',
+              title:'mosaku',
+              url:'http://task.hibimosaku.jp/',
+              is_finished:'true',
+              description:'aaaaaaaaa',
+            },
+        ],
+        
+      },
+  
+
+
     };
   },
   props:{
@@ -42,20 +157,189 @@ var tabSkill={
         <section class="tabs-content">
           <div id="slide">
             <section v-show="activeTab ==='tabs-1'">
-                <h3>現状1</h3>
-                <h3>現状1</h3>
-                <h3>現状1</h3>
-                <h3>現状1</h3>
+
+
+
+              <select name="task_progress" size="1"
+                v-model="activeoption"
+              >
+                <option 
+                  value="0">
+                  {{task_progress[0]}}</option>
+                <option 
+                  value="true">
+                  {{task_progress[1]}}</option>
+                <option 
+                  value="false">          
+                  {{task_progress[2]}}</option>
+              </select>
+                <br>
+              <table>
+                  <tr>
+                    <th>NO</th>
+                    <th>学習項目</th>
+                    <th>内容</th>
+                    <th>作品</th>
+                    <th>完了日付</th>
+
+                  </tr>
+                  <tr v-for="task in tasks.coder"
+                      v-if="task.is_finished===activeoption"
+                  >
+                    <td>{{task.number}}</td>
+                    <td>{{task.study_item}}</td>
+                    <td>{{task.description}}</td>
+                    <td><a v-bind:href="task.url">{{task.title}}</a></td>
+                    <td>{{task.date}}</td>
+                  </tr>
+                  <tr v-for="task in tasks.coder"
+                      v-if="activeoption==='0'"
+                  >
+                    <td>{{task.number}}</td>
+                    <td>{{task.study_item}}</td>
+                    <td>{{task.description}}</td>
+                    <td><a v-bind:href="task.url">{{task.title}}</a></td>
+                    <td>{{task.date}}</td>
+                  </tr>
+              </table>        
                 
             </section>
             <section v-show="activeTab ==='tabs-2'">
-                <h3>現状2</h3>
+
+              <select name="task_progress" size="1"
+                v-model="activeoption"
+              >
+                <option 
+                  value="0">
+                  {{task_progress[0]}}</option>
+                <option 
+                  value="true">
+                  {{task_progress[1]}}</option>
+                <option 
+                  value="false">          
+                  {{task_progress[2]}}</option>
+              </select>
+                <br>
+              <table>
+                  <tr>
+                    <th>NO</th>
+                    <th>学習項目</th>
+                    <th>内容</th>
+                    <th>作品</th>
+                    <th>完了日付</th>
+                  </tr>
+                  <tr v-for="task in tasks.javascript"
+                      v-if="task.is_finished===activeoption"
+                  >
+                    <td>{{task.number}}</td>
+                    <td>{{task.study_item}}</td>
+                    <td>{{task.description}}</td>
+                    <td><a v-bind:href="task.url">{{task.title}}</a></td>
+                    <td>{{task.date}}</td>
+                  </tr>
+                  <tr v-for="task in tasks.javascript"
+                      v-if="activeoption==='0'"
+                  >
+                    <td>{{task.number}}</td>
+                    <td>{{task.study_item}}</td>
+                    <td>{{task.description}}</td>
+                    <td><a v-bind:href="task.url">{{task.title}}</a></td>
+                    <td>{{task.date}}</td>
+                  </tr>
+              </table>        
+
             </section>
             <section v-show="activeTab ==='tabs-3'">
-                <h3>現状3</h3>
+
+                <select name="task_progress" size="1"
+                v-model="activeoption"
+              >
+                <option 
+                  value="0">
+                  {{task_progress[0]}}</option>
+                <option 
+                  value="true">
+                  {{task_progress[1]}}</option>
+                <option 
+                  value="false">          
+                  {{task_progress[2]}}</option>
+              </select>
+                <br>
+              <table>
+                  <tr>
+                    <th>NO</th>
+                    <th>学習項目</th>
+                    <th>内容</th>
+                    <th>作品</th>
+                    <th>完了日付</th>
+                  </tr>
+                  <tr v-for="task in tasks.php"
+                      v-if="task.is_finished===activeoption"
+                  >
+                    <td>{{task.number}}</td>
+                    <td>{{task.study_item}}</td>
+                    <td>{{task.description}}</td>
+                    <td><a v-bind:href="task.url">{{task.title}}</a></td>
+                    <td>{{task.date}}</td>
+                  </tr>
+                  <tr v-for="task in tasks.php"
+                      v-if="activeoption==='0'"
+                  >
+                    <td>{{task.number}}</td>
+                    <td>{{task.study_item}}</td>
+                    <td>{{task.description}}</td>
+                    <td><a v-bind:href="task.url">{{task.title}}</a></td>
+                    <td>{{task.date}}</td>
+                  </tr>
+              </table>        
+
             </section>
             <section v-show="activeTab ==='tabs-4'">
-              <h3>現状4</h3>
+
+                <select name="task_progress" size="1"
+                v-model="activeoption"
+              >
+                <option 
+                  value="0">
+                  {{task_progress[0]}}</option>
+                <option 
+                  value="true">
+                  {{task_progress[1]}}</option>
+                <option 
+                  value="false">          
+                  {{task_progress[2]}}</option>
+              </select>
+                <br>
+              <table>
+                  <tr>
+                    <th>NO</th>
+                    <th>学習項目</th>
+                    <th>内容</th>
+                    <th>作品</th>
+                    <th>完了日付</th>
+                  </tr>
+                  <tr v-for="task in tasks.other"
+                      v-if="task.is_finished===activeoption"
+                  >
+                    <td>{{task.number}}</td>
+                    <td>{{task.study_item}}</td>
+                    <td>{{task.description}}</td>
+                    <td><a v-bind:href="task.url">{{task.title}}</a></td>
+                    <td>{{task.date}}</td>
+                  </tr>
+                  <tr v-for="task in tasks.other"
+                      v-if="activeoption==='0'"
+                  >
+                    <td>{{task.number}}</td>
+                    <td>{{task.study_item}}</td>
+                    <td>{{task.description}}</td>
+                    <td><a v-bind:href="task.url">{{task.title}}</a></td>
+                    <td>{{task.date}}</td>
+                  </tr>
+              </table>        
+
+
+
             </section>
           </div>
         </section>
@@ -66,32 +350,7 @@ var tabSkill={
 new Vue({
   el:"#tab",
   data:{
-    task_progress:['すべて','完成','未完成'],
-    activeoption:"0",
-    tasks:[
-      {No:"1",
-      list:'scss',
-      url:'aaa@bbb',
-      progress:'未完成',
-      progress_key:'2'
-      },
-      {
-        No:"2",
-        list:'scああああああss',
-        url:'aaccca@bbb',
-        progress:'未完成',
-        progress_key:'2'
-      },
-      {
-        No:"3",
-        list:'scいいいいああss',
-        url:'aaccdddca@bbb',
-        progress:'完成',
-        progress_key:'1'
-      },
-    ],
-  },
-
+    },
   components:{
     "tab-skill":tabSkill,
   },
